313e1078e48ea1fc210335deb972dc0a4c8f144a
Luis Villagomez <luisv3189@gmail.com>
2022-08-18 14:31:16 -0500

    create working function playRound

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const getComputerChoice = () => {
-    const arrOfChoices = ['Rock', 'Paper', 'Scissors']
+    const arrOfChoices = ['rock', 'paper', 'scissors']
     const randomNum = Math.floor(Math.random() * 3)
     const comChoice = arrOfChoices[randomNum]
     return comChoice
 }
 
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'You both picked rock, try again'
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'You both picked paper, try again'
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'You both picked scissors, try again'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lost! Paper covers rock!'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock smashes scissors'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper covers rock'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lost! Scissors cuts paper!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lost! Rock smash scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors cut paper!'
+    }
+}
